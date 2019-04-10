e6c89a6385e82a03954fe55957a83b9159fddbe6
Scott Cranton <scott.cranton@solo.io>
2019-04-10 13:27:32 -0400

    glooctl: 0.13.13 update (#6)

--- Formula/glooctl.rb
+++ Formula/glooctl.rb
@@ -1,17 +1,17 @@
 class Glooctl < Formula
   desc "Envoy-Powered API Gateway"
   homepage "https://gloo.solo.io"
-  version "0.13.12"
+  version "0.13.13"
 
   if OS.mac?
     url "https://github.com/solo-io/gloo/releases/download/v#{version}/glooctl-darwin-amd64"
-    sha256 "b872beb2658e99912fd7825e51f37cc15c7c7a20c2c6b1a4a61860ddb9fcdb17"
+    sha256 "70e3a152f300e031719a53fa6f539ab5d5e8085a511e70451c195a59c5789f1a"
   elsif OS.linux?
     url "https://github.com/solo-io/gloo/releases/download/v#{version}/glooctl-linux-amd64"
-    sha256 "8dad04591ddd227f1d307b5c5f737bcdda8c5f24060730d19a739dc11fa146f3"
+    sha256 "9d254b250f4a6b9e05814b29141dabbd0d0af084bc175b86d954bd326d574358"
   else
     url "https://github.com/solo-io/gloo/releases/download/v#{version}/glooctl-windows-amd64.exe"
-    sha256 "6a7eb0c28434474b7627494ddcf6575fd01f5f780f2f83e9f0face22dfe43ee9"
+    sha256 "623b80693426104b8887c72257528ae0576ba5571cea4d34c9a2b5dab6947c34"
   end
 
   def install
